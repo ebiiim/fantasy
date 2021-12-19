860a18168e1e5a7c703a9c2ca2c061cb17e4cc9d
ebiiim <mail@ebiiim.com>
2021-12-19 23:05:20 +0900

    wip: OBJ_BG = 2

--- base/object_types.go
+++ base/object_types.go
@@ -3,11 +3,11 @@ package base
 const (
 	OBJ_Err   ObjectType = iota // 0
 	OBJ_None                    // 1
-	OBJ_Base                    // 2
-	OBJ_Grass                   // 3
-	OBJ_Tree                    // 4
-	OBJ_Box                     // 5
+	OBJ_BG                      // 2
+	OBJ_Base                    // 3
+	OBJ_Grass                   // 4
+	OBJ_Tree                    // 5
+	OBJ_Box                     // 6
 
 	OBJ_Me ObjectType = -1
-	OBJ_BG ObjectType = -99999999
 )
