2af92a900a7e7511f08065ee32c8f66fb6c2d104
ebiiim <mail@ebiiim.com>
2021-12-21 19:22:51 +0900

    refactor: naming

--- base/vertex.go
+++ base/vertex.go
@@ -25,14 +25,14 @@ func (v0 Vertex) Div(v Vertex) Vertex {
 	return Vertex{v0.X / v.X, v0.Y / v.Y}
 }
 
-func (v Vertex) ToIndex(dim Vertex) int {
-	return dim.X*v.Y + v.X
+func (v0 Vertex) ToIndex(dim Vertex) int {
+	return dim.X*v0.Y + v0.X
 }
 
 func VertexFromIndex(dim Vertex, idx int) Vertex {
 	return Vertex{idx % dim.X, idx / dim.X}
 }
 
-func (v Vertex) IsOutside(dim Vertex) bool {
-	return v.X < 0 || v.Y < 0 || v.X >= dim.X || v.Y >= dim.Y
+func (v0 Vertex) IsOutside(dim Vertex) bool {
+	return v0.X < 0 || v0.Y < 0 || v0.X >= dim.X || v0.Y >= dim.Y
 }
